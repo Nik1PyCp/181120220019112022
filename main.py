448b19231976a0b0aabab0b517cb4fcdf187dc2c
Veronika <vkotsubiak@gmail.com>
2022-11-18 20:36:29 +0200

    ТАБЛИЦЯ

---
+++ main.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+
+p = input('Enter 3 for create\nEnter 2 for delete\nEnter 1 for check\n')
+connection = sqlite3.connect('products.sl3', 5)
+cur = connection.cursor()
+#cur.execute('CREATE TABLE products (name TEXT, price TEXT, number TEXT)')
+#connection.commit()
+
